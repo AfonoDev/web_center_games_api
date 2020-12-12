795eaeafc88baf6a75af43bc306e0877d3acf367
DevLoopStar <56160428+DevLoopStar@users.noreply.github.com>
2020-12-12 15:16:40 -0300

    Correção Conexão Postgres

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,10 @@ module.exports = {
       url: process.env.DATABASE_URL ||  
     "postgres://postgres:bcd127@localhost:5432/dbGamesCenter", 
       config: { 
-        dialect: "postgres", 
+        dialect: "postgres",
+        "dialectOptions": {
+            "ssl": true
+        },          
         logging: console.log, 
         define: { 
           timestamp: true, 
